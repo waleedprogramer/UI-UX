ff9a941a4f6e15d9f8a30a27422fb0a2afafeb3b
Awaisprogram <awaisbinmehmoodahmed@gmail.com>
2024-12-10 02:49:05 +0500

    fix

--- src/app/components/BlogPage.tsx
+++ src/app/components/BlogPage.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 import Image from "next/image";
-import b1 from '../Images2/b1.png'
-import b2 from '../Images2/b2.png'
-import b3 from '../Images2/b3.png'
+import b1 from '../Images2/blog1.png'
+import b2 from '../Images2/blog2.png'
+import b3 from '../Images2/blog3.png'
 import { FaPenNib } from "react-icons/fa";
 import { SlCalender } from "react-icons/sl";
 
